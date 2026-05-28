a6ea681cc1abf5e76719409ca551bc0a6d32daed
Aditya Verma <adityaver2412@gmail.com>
2026-05-28 14:14:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,6 @@ import {
   Montserrat,
 } from 'next/font/google';
 
-import Script from 'next/script';
-
 import { Analytics } from '@vercel/analytics/next';
 
 import './globals.css';
@@ -194,23 +192,28 @@ export default function RootLayout({
       lang="en"
       className={`${poppins.variable} ${montserrat.variable}`}
     >
+
+      <head>
+
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wy2vj0utmw");
+            `,
+          }}
+        />
+
+      </head>
+
       <body className="font-sans antialiased bg-cream">
 
         {children}
-
-        {/* Microsoft Clarity */}
-        <Script
-          id="microsoft-clarity"
-          strategy="afterInteractive"
-        >
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "wy2vj0utmw");
-          `}
-        </Script>
 
         {/* Vercel Analytics */}
         {process.env
